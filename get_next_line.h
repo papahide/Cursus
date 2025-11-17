c4efe616cb9aa9f67a5cfae338a4ac915604ab09
papahide <pavelapahidean@gmail.com>
2025-11-17 20:10:59 +0100

    17-11-2025 - Changes on GNL and tests

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paapahid <paapahid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 21:18:36 by paapahid          #+#    #+#             */
+/*   Updated: 2025/11/05 13:15:26 by paapahid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+int		ft_complete_line(char *str);
+char	*ft_strjoin(char *s1, const char *s2);
+char	*ft_getline_gnl(char **str);
+
+#endif
